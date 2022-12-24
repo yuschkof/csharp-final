9c7274a769c6b15b1dec1df086fe56949ea610dc
Anton <yuschkof.toscha@yandex.ru>
2022-12-24 17:45:00 +0400

    fix

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -2,7 +2,6 @@
 {
     static void Main(string[] args)
     {
-        // Print the multiplication table
         for (int i = 1; i <= 10; i++)
         {
             for (int j = 1; j <= 10; j++)
--- isPointInside/Program.cs
+++ isPointInside/Program.cs
@@ -4,7 +4,6 @@ public class isPointInside
 {
     static void Main(string[] args)
     {
-        // Prompt the user to enter the x and y coordinates of the point
         Console.WriteLine("Введите координату x точки:");
         double x = double.Parse(Console.ReadLine());
         Console.WriteLine("Введите координату y точки:");
